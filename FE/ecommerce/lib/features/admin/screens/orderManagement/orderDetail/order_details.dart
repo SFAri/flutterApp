7511765186ebd0f04d33f3fe1a4cb581b63908d3
AnhKietTruong <anhkiettruong6803@gmail.com>
2025-05-11 20:56:26 +0700

    Create order_details.dart
     (haven't done anything yet)

---
+++ FE/ecommerce/lib/features/admin/screens/orderManagement/orderDetail/order_details.dart
@@ -0,0 +1,19 @@
+// 12/5 DO IT
+
+import 'package:ecommerce/common/widgets/form/custom_dialogform.dart';
+import 'package:ecommerce/common/widgets/form/custom_textformfield.dart';
+import 'package:ecommerce/features/admin/responsive.dart';
+import 'package:ecommerce/features/admin/screens/dashboard/widgets/header.dart';
+import 'package:ecommerce/utils/constants/image_strings.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// class OrderDetailScreen extends StatefulWidget {
+//   const OrderDetailScreen(Map<String, dynamic> item, {super.key});
+//
+//   @override
+//   State<StatefulWidget> createState() => _OrdertDetailScreenState();
+// }
+// class _OrdertDetailScreenState() extends State<OrderDetailScreen>{
+//
+// }
